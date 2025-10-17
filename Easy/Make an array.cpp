d7b9b808c21f3a5f7e337cf763a8e5291c080fa1
Pankaj Bashera <146298510+Pankaj-Bashera@users.noreply.github.com>
2025-10-17 13:45:43 +0530

    Add solution for array problem in Make an array.cpp
    Implement a function to solve the array problem based on the sum and maximum element conditions.

---
+++ Easy/Make an array.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+int solve (int N, vector<int> A) {
+   // Write your code here
+   long long sum = accumulate(A.begin(), A.end(), 0ll);
+   if(sum%(N-1)!=0) return -1;
+
+   int k = sum/(N-1);
+   int mx = *max_element(A.begin(), A.end());
+   return mx<=k ? k : -1;
+}
+
+int main() {
+
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int T;
+    cin >> T;
+    for(int t_i = 0; t_i < T; t_i++)
+    {
+        int N;
+        cin >> N;
+        vector<int> A(N);
+        for(int i_A = 0; i_A < N; i_A++)
+        {
+        	cin >> A[i_A];
+        }
+
+        int out_;
+        out_ = solve(N, A);
+        cout << out_;
+        cout << "\n";
+    }
+}
